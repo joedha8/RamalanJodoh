082a4d4d92134817ff9c8d47acfe5ae41103f481
Blank15 <yudistirosaputro@gmail.com>
2017-01-01 20:45:10 +0700

    Layout detail

---
+++ app/src/main/java/com/kudkud32/ramalanjodoh/Detail.java
@@ -0,0 +1,13 @@
+package com.kudkud32.ramalanjodoh;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Detail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
